85ebe1c57e2fffa678b0712927b927982ef21d26
Cas du Plessis <go4cas@gmail.com>
2021-02-25 21:03:59 +0200

    Clean up index.js

--- src/index.js
+++ src/index.js
@@ -10,13 +10,3 @@ const stores = import.meta.globEager('./components/**/store.js')
   await import('alpine-magic-helpers')
   await import('alpinejs')
 })()
-
-/*
-document.querySelector('#app').innerHTML = `
-<div class="w-40 h-40 bg-blue-300 text-white">hello</div>
-<div class="w-40 h-40 bg-red-300 text-white">hello2</div>
-<div class="w-40 h-40 bg-green-300 text-white">hello3</div>
-<div class="w-40 h-40 bg-gray-300 text-white">hello4</div>
-<div class="w-40 h-40 bg-blue-300 text-white">hello5</div>
-`
-*/
